34becc7031e02e9a7b5d27e8949c554cd0495191
henzo <84385799+henzopuchetti@users.noreply.github.com>
2025-08-25 08:44:51 -0300

    Update Program.cs

--- Biblioteca.Api/Program.cs
+++ Biblioteca.Api/Program.cs
@@ -3,20 +3,16 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
 builder.Services.AddControllers();
 
-// Configurar Swagger/Swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configurar DbContext com Oracle (pega string do appsettings.json)
 builder.Services.AddDbContext<BibliotecaDbContext>(options =>
     options.UseOracle(builder.Configuration.GetConnectionString("OracleDb")));
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -25,7 +21,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// Ativar Controllers (API estilo REST)
 app.MapControllers();
 
 app.Run();
